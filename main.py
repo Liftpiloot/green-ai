36479cd13f37e9d9e73dac534b923b1aa685bc2c
Abel <vandijkabel@gmail.com>
2025-06-05 15:12:51 +0200

    ff

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ AI_API_KEY = os.getenv("GeminiAPI")
 async def generate_challenge(location: str = None, information: str = None):
     if not AI_API_KEY:
         return {"error": "AI API key not found"}
-    content = f"Generate a short and actionable challenge for improving the environment (max 20 words) for users near {location}, where {information}. Keep it fun and motivating. examples are, plant a tree, plant a hedge or a flower, etc. Be very exact, the user should know exactly what to do. if talking about a tree, be specific about the type of tree, if talking about a flower, be specific about the type of flower. Do not use any other words than the challenge itself. The locations are public areas. Also state the reason, which metric will it improve"
+    content = f"Generate a short and actionable challenge for improving the environment (max 20 words) for users near {location}, where {information}. Keep it fun and motivating. examples are, plant a tree, plant a hedge or a flower, etc. Be very exact, the user should know exactly what to do. if talking about a tree, be specific about the type of tree, if talking about a flower, be specific about the type  of flower. Do not use any other words than the challenge itself. The locations are public areas. Also state the reason, which metric will it improve"
     client = genai.Client(api_key=AI_API_KEY)
     print("content: " + content)
     try:
